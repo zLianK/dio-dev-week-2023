ce88f397bb61d7a5d4f1db1fd12c00c688dea601
Lían König <lk.liankonig@gmail.com>
2023-04-16 22:26:40 -0300

    third class

--- src/app/components/new-post/new-post.component.ts
+++ src/app/components/new-post/new-post.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MessagesService } from 'src/app/services/messages.service';
 
 @Component({
   selector: 'app-new-post',
@@ -10,12 +11,16 @@ export class NewPostComponent {
   tweetMaxLength: number = 240
   tweetRemain: number = this.tweetMaxLength
 
+  constructor(private service: MessagesService) { }
+
   sendTweet() {
-    alert(this.tweetMessage)
+    this.service.addMessage(this.tweetMessage)
+    this.tweetMessage = ""
+    this.tweetRemain = this.tweetMaxLength
   }
 
   changeTweet() {
-    this.tweetRemain = 240 - this.tweetMessage.length
+    this.tweetRemain = this.tweetMaxLength - this.tweetMessage.length
   }
 
 }
